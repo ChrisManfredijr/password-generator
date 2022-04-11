a2a1fae90a78b31605b6ede863a1fbda64817f8d
Chris <chrismanfredijr@gmail.com>
2022-04-11 12:22:22 -0400

    added character generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,37 @@
 // Assignment code here
+function generatePassword() {
+  var characterPool = "";
+  var lowerCase = "abcdefghijklmnopqrstupwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberCase = "1234567890";
+  var specialCase = " !”#$%&’()*+,-./:;<=>?@[\]^_`{|}~";
+  
+  characterGenerator();
 
+  //this function asks the user what characters to include in the password, if no characters are selected, the function repeats
+  function characterGenerator() {
+    if(window.confirm("Include lowercase letters?")){
+      characterPool += lowerCase;
+    }
+  
+    if(window.confirm("Include uppercase letters?")){
+      characterPool += upperCase;
+    }
 
+    if(window.confirm("Include numbers?")){
+      characterPool += numberCase;
+    }
+
+    if(window.confirm("Include special characters?")){
+      characterPool += specialCase;
+    }
+
+    if(characterPool === ""){
+      window.alert("You did not select any character types! please try again")
+      characterGenerator();
+    }
+  }
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
